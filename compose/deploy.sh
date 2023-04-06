d2f44a49ed03f30aa92718454db0a16fe11e4727
Javier del Aguila <fraagol@users.noreply.github.com>
2023-04-06 13:36:04 +0200

    Update deploy.sh

--- compose/deploy.sh
+++ compose/deploy.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+echo
 echo docker stack rm volume-test
 docker stack rm volume-test
 
+echo
 echo docker stack deploy -c ./docker-compose.yaml volume-test 
 docker stack deploy -c ./docker-compose.yaml volume-test 
 
+echo
 echo docker service ls
 docker service ls
