4d8bf9adffb1c2fcc5bf189eec693122b96c9d9d
Javi del Aguila <jdelaguila.externo@valenciaport.com>
2023-05-18 19:16:41 +0200

    script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 echo
 echo git pull
+git pull
+echo
+echo chmod +x /home/mimacom/elastic/scripts/deploy.sh
+chmod +x /home/mimacom/elastic/scripts/deploy.sh
 echo
 echo docker stack rm volume-test
 docker stack rm volume-test
